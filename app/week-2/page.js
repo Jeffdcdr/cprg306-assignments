b47cbff71c85b416c88d940be9097a342900ffee
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-09-11 20:09:59 -0600

    Additional page and codes
    Added student-info page and imported it to the week-2 page so it renders the information from the other page

---
+++ app/week-2/page.js
@@ -0,0 +1,9 @@
+import Studentinfo from "./student-info";
+export default function Page() {
+    return (
+      <main>
+        <h1>Shopping List</h1>
+        <Studentinfo/>
+      </main>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Studentinfo() {
+    return (
+      <main>
+        <h1>Jeffry Ancheta</h1>
+        <Link href="https://github.com/Jeffdcdr?tab=repositories">My github repo link</Link>
+      </main>
+    );
+  }
