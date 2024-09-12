1a6b7442927e5350b0d654947884c1623e712dcd
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-09-11 20:41:23 -0600

    Additional commits
    Home page updated
    Added back button - underlined
    Added some formatting to make it look a little bit better

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import Studentinfo from "./student-info";
 export default function Page() {
     return (
       <main>
         <h1>Shopping List</h1>
         <Studentinfo/>
+        <p>
+        Go back to <Link href="/" className="underline">Home page</Link>
+        </p>
       </main>
+      
+      
     );
   }
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,9 @@ export default function Studentinfo() {
     return (
       <main>
         <h1>Jeffry Ancheta</h1>
+        <li>
         <Link href="https://github.com/Jeffdcdr?tab=repositories">My github repo link</Link>
+        </li>
       </main>
     );
   }
