087efd77a7af3a722786c1d3661d2252d266de37
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-09-23 21:11:18 -0600

    Update page.js
    additional Styling

--- app/page.js
+++ app/page.js
@@ -4,20 +4,35 @@ import Page from "./week-2/page";
 export default function Home(){
   return (
     <header className="text-2xl mx-4 my-4">
-      CPRG 306: Web Development 2           
-      <h2>Links to my assignments</h2>             
+      CPRG 306: Web Development 2<h2>Links to my assignments</h2>
       <br></br>
-    <div >
-      <li><Link href="/week-2/">Week 2</Link> </li>
-      <li className="hover:underline  decoration-red-700" ><Link href="/week-3/">Week 3</Link> </li>            
-      <li><Link href="/week-4/">Week 4</Link> </li>            
-      <li><Link href="/week-5/">Week 5</Link> </li>            
-      <li><Link href="/week-6/">Week 6</Link> </li>            
-      <li><Link href="/week-7/">Week 7</Link> </li>            
-      <li><Link href="/week-8/">Week 8</Link> </li>            
-      <li><Link href="/week-9/">Week 9</Link> </li>
-    </div>
-    </header>  
+      <div>
+        <li className="hover:bg-amber-200 hover:text-stone-900 max-w-xs rounded-full">
+          <Link href="/week-2/">Week 2</Link>
+        </li>
+        <li className="hover:underline  decoration-red-700">
+          <Link href="/week-3/">Week 3</Link>
+        </li>
+        <li>
+          <Link href="/week-4/">Week 4</Link>{" "}
+        </li>
+        <li>
+          <Link href="/week-5/">Week 5</Link>{" "}
+        </li>
+        <li>
+          <Link href="/week-6/">Week 6</Link>{" "}
+        </li>
+        <li>
+          <Link href="/week-7/">Week 7</Link>{" "}
+        </li>
+        <li>
+          <Link href="/week-8/">Week 8</Link>{" "}
+        </li>
+        <li>
+          <Link href="/week-9/">Week 9</Link>{" "}
+        </li>
+      </div>
+    </header>
   );
 }
 
