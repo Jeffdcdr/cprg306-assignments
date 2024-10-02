33f88f2d1aedb0fe7d3229947699f025caa2a4e5
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-02 01:56:02 -0600

    Update new-item.js
    Formatting using ALT + SHIFT + F

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -3,48 +3,61 @@
 import { useState } from "react";
 
 export default function NewItem() {
-    
-    let [quantity, setQuantity] = useState(1);
-    
-    // for increment function
-    let increment = () => {
-        if (quantity < 20) 
-            setQuantity(quantity + 1)}; 
+  let [quantity, setQuantity] = useState(1);
 
-    // for decrement function
-    let decrement = () => { 
-        if (quantity > 1)
-            setQuantity(quantity - 1)};
+  // for increment function
+  let increment = () => {
+    if (quantity < 20) setQuantity(quantity + 1);
+  };
 
-    // for reset function
-    const reset = () => setQuantity(1);
+  // for decrement function
+  let decrement = () => {
+    if (quantity > 1) setQuantity(quantity - 1);
+  };
 
-    return (
+  
+
+
+  // for reset function
+  const reset = () => setQuantity(1);
+
+  return (
     <div>
-        <h3>Current Quantity: {quantity}</h3>
-        <br></br>
+      <h3>Current Quantity: {quantity}</h3>
+      <br></br>
 
-        <button 
-        onClick={increment} 
-        // className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded"  
+      <button
+        onClick={increment}
+        // className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded"
         className={`${
-            quantity === 20 ? "bg-gray-500 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-700"
-          } text-white font-bold py-2 px-2 rounded`}
-        //   disabled={quantity === 20}      
-        >Increment</button>
+          quantity === 20
+            ? "bg-gray-500 cursor-not-allowed"
+            : "bg-blue-500 hover:bg-blue-700"
+        } text-white font-bold py-2 px-2 rounded`}
+        //   disabled={quantity === 20}
+      >
+        Increment
+      </button>
 
-        <button
-        onClick={decrement} 
+      <button
+        onClick={decrement}
         // className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-2 rounded"
         className={`${
-            quantity === 1 ? "bg-gray-500 cursor-not-allowed" : "bg-red-500 hover:bg-red-700"
-          } text-white font-bold py-2 px-2 rounded`}
+          quantity === 1
+            ? "bg-gray-500 cursor-not-allowed"
+            : "bg-red-500 hover:bg-red-700"
+        } text-white font-bold py-2 px-2 rounded`}
         //   disabled={quantity === 1}
-        >Decrement</button>
+      >
+        Decrement
+      </button>
 
-        <button
-        onClick={reset} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-2 rounded"        
-        >Reset</button>
+      <button
+        onClick={reset}
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-2 rounded"
+      >
+        Reset
+      </button>
     </div>
-);
+  );
 }
