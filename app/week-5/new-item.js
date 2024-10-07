542262a483c9e5979d368d19fecd5a781d9fd845
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-07 01:53:34 -0600

    Week-5 folder created
    copied week-4 items per instructions
    adding use state hooks for name and category

---
+++ app/week-5/new-item.js
@@ -0,0 +1,70 @@
+"use client";
+
+import { useState } from "react";
+
+export default function NewItem() {
+  let [quantity, setQuantity] = useState(1);
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("produce");
+
+  // for increment function
+  // let increment = () => {
+  //   if (quantity < 20) setQuantity(quantity + 1);
+  // };
+  
+  let increment = () => quantity < 20? setQuantity(quantity + 1) : null;
+
+  // for decrement function
+  //   let decrement = () => {
+  //     if (quantity > 1) setQuantity(quantity - 1);
+  //   };
+
+  let decrement = () => quantity > 1 ? setQuantity(quantity - 1) : null;
+  
+
+
+  // for reset function
+  const reset = () => setQuantity(1);
+
+  return (
+    <div>
+      <h3>Current Quantity: {quantity}</h3>
+      <br></br>
+
+      <button
+        onClick={increment}
+        // className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded"
+        className={`${
+          quantity === 20
+            ? "bg-gray-500 cursor-not-allowed"
+            : "bg-blue-500 hover:bg-blue-700"
+        } text-white font-bold py-2 px-2 rounded`}
+        //   disabled={quantity === 20}
+      >
+        {/* Increment */}
+        +
+      </button>
+
+      <button
+        onClick={decrement}
+        // className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-2 rounded"
+        className={`${
+          quantity === 1
+            ? "bg-gray-500 cursor-not-allowed"
+            : "bg-red-500 hover:bg-red-700"
+        } text-white font-bold py-2 px-2 rounded`}
+        //   disabled={quantity === 1}
+      >
+        {/* Decrement */}
+        +
+      </button>
+
+      <button
+        onClick={reset}
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-2 rounded"
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
