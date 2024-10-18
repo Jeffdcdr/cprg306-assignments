c71d1a6c7c0ed48e972c37c296a8198bfe595962
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-18 14:48:04 -0600

    Update page.js
    Just an update to the main page formatting

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,15 +1,17 @@
 import Link from "next/link";
+import ItemList from "./item-list";
+import Item from "./items";
 
 
 export default function Page() {
     return (
       <main>
-        <h1 className="text-2xl mx-4 my-4">Shopping List</h1>
-        
+        <h1 className="m-4 text-3xl font-bold">Shopping List</h1>
+        <ItemList/>
         <br></br>
-        <p>
-        Go back to <Link href="/" className="underline">Home page</Link>
-        </p>
+        <div className="ml-4">
+        Go back to <Link href="/" className="underline" >Home page</Link>
+        </div>
       </main>
     );
 }
