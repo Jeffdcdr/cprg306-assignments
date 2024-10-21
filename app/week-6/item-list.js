251c2ec040ae836c88521af83f3591c8d2deed44
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-10-20 23:54:29 -0600

    Update item-list.js
    added the button colors functionality while it is being selected

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -5,6 +5,7 @@ import itemsJson from "./items.json";
 
 export default function ItemList() {
   const [sortby, setSortby] = useState("name");
+  const [groupedCategory, setGroupedCategory] = useState(false); // added to handle grouped category
 
   // let items = [...itemsJson] - original code to render what's on the JSON file
 
@@ -19,49 +20,94 @@ export default function ItemList() {
     }
   });
 
+  const handleGroupedCategory = () => {
+    const groupedItems = items.reduce((accumulator, item) => {
+      if (!accumulator[item.category]) {
+        accumulator[item.category] = [];
+      }
+      accumulator[item.category].push(item);
+      return accumulator;
+    }, {});
+    setGroupedCategory(groupedItems); // added to set grouped category
+    setSortby("grouped category");
+    console.log(groupedItems)
+    
+  };
+
   const handleItemClick = (value) => {
     setSortby(value);
+    setGroupedCategory(false);
   };
 
   return (
     <div>
       <div className="ml-4">
-        <label htmlFor="sortby">Sort by:</label>
+        <label htmlFor="sortby" className="text-xl">Sort by:</label>
         <input
           type="button"
           value="Name"
           // className="bg-green-600 p-1 m-2 w-28"
-          className={`p-1 m-2 w-28 ${sortby === "name" ? "bg-green-600" : "bg-green-800"}`}
-            //  className={`p-1 m-2 w-28 ${sortby === value ? "bg-green-600" : "bg-green-800"}`}
+          className={`p-1 m-2 w-24 rounded-md ${
+            sortby === "name" ? "bg-sky-600" : "bg-sky-800"
+          }`}
+          //  className={`p-1 m-2 w-28 ${sortby === value ? "bg-green-600" : "bg-green-800"}`}
 
           // onClick={() => setSortby("name")}
           onClick={() => handleItemClick("name")}
-        ></input>
+        />
         <input
           type="button"
           value="Category"
           // className="bg-green-600 p-1 m-2 w-28"
-          className={`p-1 m-2 w-28 ${sortby === "category" ? "bg-green-600" : "bg-green-800"}`}
+          className={`p-1 m-2 w-24 rounded-md ${
+            sortby === "category" ? "bg-sky-600" : "bg-sky-800"
+          }`}
           // onClick={() => setSortby("category")}
           onClick={() => handleItemClick("category")}
-        ></input>
+        />
         <input
           type="button"
           value="Grouped Category"
-          className="bg-green-800 p-1 m-2 w-28 h-10"
-        ></input>
+          className={`p-1 m-2 w-40 rounded-md ${
+            sortby === "grouped category" ? "bg-sky-600" : "bg-sky-800"
+          }`}
+          
+          onClick={() => handleGroupedCategory("grouped category")}
+        />
       </div>
 
-      <ul>
-        {items.map((item) => (
-          <li key={item.id} className="p-2 m-4 bg-zinc-900 max-w-sm">
-            <h2 className="text-xl font-semibold"> {item.name} </h2>
-            <div className="text-sm">
-              Buy {item.quantity} in {item.category}
-            </div>
-          </li>
-        ))}
-      </ul>
+      {groupedCategory ? (
+        <div>
+          <ul>
+            {Object.keys(groupedCategory).map((category) => (
+              <li key={category} >
+                <h3 className="p-2 m-4 text-sky-400 text-transform: capitalize text-2xl">{category}</h3>
+                <ul>
+                  {groupedCategory[category].map((item) => (
+                    <li key={item.id} className="p-2 m-4 bg-zinc-900 max-w-sm">
+                      <h2 className="text-xl font-semibold"> {item.name} </h2>
+                      <div className="text-sm">
+                        Buy {item.quantity} in {item.category}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <ul>
+          {items.map((item) => (
+            <li key={item.id} className="p-2 m-4 bg-zinc-900 max-w-sm">
+              <h2 className="text-xl font-semibold"> {item.name} </h2>
+              <div className="text-sm">
+                Buy {item.quantity} in {item.category}
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
