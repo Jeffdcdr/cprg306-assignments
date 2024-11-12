025fba988053eeb7fb844ba8d42c59c992277a52
Jeff <61839297+Jeffdcdr@users.noreply.github.com>
2024-11-12 01:13:15 -0700

    Update meal-ideas.js

--- app/week-8/meal-ideas.js
+++ app/week-8/meal-ideas.js
@@ -5,7 +5,6 @@ import Image from "next/image";
 
 export default function MealIdeas({ ingredient }) {
   const [meals, setMeals] = useState([]);
-  
 
   async function fetchMealIdeas(ingredient) {
     const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`;
@@ -13,49 +12,47 @@ export default function MealIdeas({ ingredient }) {
       const response = await fetch(url);
       const data = await response.json();
       setMeals(data && data.meals ? data.meals : []);
-      
     } catch (error) {
       console.error("Error fetching meal ideas", error);
       setMeals([]);
-      
     }
-  }
 
-  // const loadMealIdeas = () => {
-  //   //   fetchMealIdeas(ingredient);
-  //   setMeals(meals);
-  // };
+    // const loadMealIdeas = () => {
+    //   //   fetchMealIdeas(ingredient);
+    //   setMeals(meals);
+    // };
 
-  useEffect(() => {
-    // loadMealIdeas(ingredient);
-    fetchMealIdeas(ingredient);
-  }, [ingredient]);
+    useEffect(() => {
+      // loadMealIdeas(ingredient);
+      fetchMealIdeas(ingredient);
+    }, [ingredient]);
 
-  return (
-    <div>
-      {meals.length > 0 ? (
-        <ul>
+    return (
+      <div>
+        {meals.length > 0 ? (
+          <ul>
+            <p className="text-2xl capitalize">
+              Here are some meal ideas using {ingredient}
+            </p>
+
+            {meals.map((meal) => (
+              <li key={meal.idMeal}>
+                <h3>{meal.strMeal}</h3>
+                <img
+                  src={meal.strMealThumb}
+                  alt={meal.strMeal}
+                  width={150}
+                  height={150}
+                />
+              </li>
+            ))}
+          </ul>
+        ) : (
           <p className="text-2xl capitalize">
-            Here are some meal ideas using {ingredient}
+            No meal ideas available for &quot;{ingredient}&quot;.
           </p>
-
-          {meals.map((meal) => (
-            <li key={meal.idMeal}>
-              <h3>{meal.strMeal}</h3>
-              <img
-                src={meal.strMealThumb}
-                alt={meal.strMeal}
-                width={150}
-                height={150}
-              />
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p className="text-2xl capitalize">
-          No meal ideas available for &quot;{ingredient}&quot;.
-        </p>
-      )}
-    </div>
-  );
+        )}
+      </div>
+    );
+  }
 }
